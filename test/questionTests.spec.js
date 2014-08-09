2b6e0544414b0042c262dfc410e2bc31fbaff62c
krustanov <pesho1994@gmail.com>
2014-08-09 14:47:09 +0300

    Some more tests

--- test/questionTests.spec.js
+++ test/questionTests.spec.js
@@ -5,4 +5,11 @@ describe('Question Page Tests', function () {
         browser.get('http://localhost:8080/#/test/start');
     });
 
+    it('should open pop-up box', function () {
+        element.all(by.binding('answer')).then(function (questions) {
+            expect(questions.length).toBe(5);
+            questions[0].click();
+            expect(element(by.binding('data[questionNumber].answer')).isDisplayed()).toBe(true);
+        })
+    });
 });
--- test/startPageTests.spec.js
+++ test/startPageTests.spec.js
@@ -1,5 +1,6 @@
 
 describe('JavaScript Test Start Page', function () {
+    var ptor = protractor.getInstance();
 
     beforeEach(function() {
         browser.get('http://localhost:8080/');
@@ -7,6 +8,14 @@ describe('JavaScript Test Start Page', function () {
 
     it('should have a title', function() {
         expect(browser.getTitle()).toEqual('JavaScript Test');
+    });
+
+    it('should go to next page', function () {
+        element(by.id('start-button')).click();
+        browser.waitForAngular();
+        ptor.getCurrentUrl().then(function(url){
+            expect(url).toEqual('http://localhost:8080/#/test/start')
+        });
     });
 
 });
